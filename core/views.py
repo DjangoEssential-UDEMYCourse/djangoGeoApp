791e9d9e3c4ffc454760eeade0d26d2a486a7909
THUNDER MASTER <thndrmaster@gmail.com>
2021-08-16 10:17:30 -0300

    [AULA 99] - | /CRIANDO A VIEW INDEX NO DJANGO GEO APP\ |

--- core/views.py
+++ core/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View
+from .utils import yelp_search, get_client_data
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        itens = []
+        city = None
+        location = city
+
+        while not city:
+            ret = get_client_data()
+            city = ret['city']
+
+        resource = request.GET.get('key', None)
+        loc = request.GET.get('loc', None)
+
+        context = {
+            'city': city,
+            'busca': False,
+        }
+
+        if loc:
+            location = loc
+        if resource:
+            itens = yelp_search(keyword=resource, location=location)
+            context = {
+                'itens': itens,
+                'city': location,
+                'busca': True
+            }
+        return render(request, 'index.html', context)
